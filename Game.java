0b4441b1f8170730f41fc6164958887061a6b718
LiuJunxian <110192238+MisterJunxian@users.noreply.github.com>
2022-12-26 21:56:39 +0800

    启动坦克大战

---
+++ Game.java
@@ -0,0 +1,21 @@
+package tankbattle;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+    public static void main(String[] args) {
+        new Game();
+    }
+    
+    public Game() {
+        MyPanel mp = new MyPanel();
+        Thread thread = new Thread(mp);
+        thread.start();
+        this.add(mp);
+        this.addKeyListener(mp);
+        this.setSize(1230, 1000);
+        this.setTitle("坦克大战");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
