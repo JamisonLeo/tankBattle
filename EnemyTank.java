d23ec78d7eba9af613783fbd7b9ebd179f3acca9
LiuJunxian <110192238+MisterJunxian@users.noreply.github.com>
2022-12-26 21:56:14 +0800

    敌人坦克

---
+++ EnemyTank.java
@@ -0,0 +1,83 @@
+package tankbattle;
+
+public class EnemyTank extends Tank implements Runnable {
+    
+    public EnemyTank(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (isLive() && MyPanel.getShots().size() < 3) {
+                if(((int)(Math.random() * 10 + 1)) <= 7) {
+                    Shot shot = shot();
+                    MyPanel.getShots().add(shot);
+                    new Thread(shot).start();
+                }
+            }
+
+            switch (getDirection()) {
+                case UP:
+                for (int i = 0; i < 70; i++){
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (getY() > 20) moveUp();
+                    else break;
+                }
+                    break;
+                case DOWN:
+                for (int i = 0; i < 70; i++){
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (getY() < 830) moveDown();
+                    else break;
+                }
+                    break;
+                case LEFT:
+                for (int i = 0; i < 70; i++){
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (getX() > 40) moveLeft();
+                    else break;
+                }
+                    break;
+                case RIGHT:
+                for (int i = 0; i < 70; i++){
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (getX() < 1106) moveRight();
+                    else break;
+                }
+                    break;
+            }
+            
+            switch ((int)(Math.random() * 4)) {
+                case 0:
+                    setDirection(Direction.UP);
+                    break;
+                case 1:
+                    setDirection(Direction.DOWN);
+                    break;
+                case 2:
+                    setDirection(Direction.LEFT);
+                    break;
+                case 3:
+                    setDirection(Direction.RIGHT);
+                    break;
+            }
+        }
+    }
+}
